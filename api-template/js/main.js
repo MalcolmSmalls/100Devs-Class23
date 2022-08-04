71bff8d22fceb21d04f96ded71615c5f907000c3
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-04 14:26:05 -0700

    Finished extracting data from API and storing it into local storage

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -4,11 +4,12 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://openlibrary.org/works/${choice}.json`
+  const url = `https://openlibrary.org/isbn/${choice}.json`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        localStorage.setItem('Title', data.title)
         console.log(data)
 
       })
