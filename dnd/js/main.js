85750957d5640f26773115b96a01a3eebba50f3d
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-04 12:11:12 -0700

    Finished gathering necessary API Data and iterating to add a list to UL on DnD objects

--- dnd/js/main.js
+++ dnd/js/main.js
@@ -2,13 +2,20 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
+  let choice = document.querySelector('input').value
+  if(choice.includes(" ") === true){
+    choice = choice.split(" ").join("-")
+  }
   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+       console.log(data)
+       document.querySelector('.name').innerText = data.name
+       document.querySelector('.class').innerText = data.classes[0].name
+       for(i=0; i < data.subclasses.length; i++){  
+        document.querySelector('ul').appendChild(document.createElement('li')).textContent = data.subclasses[i].name}
       })
       .catch(err => {
           console.log(`error ${err}`)
